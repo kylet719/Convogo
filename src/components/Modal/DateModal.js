f126e697b328d084b7617ac36ae594b4269c4c12
Alethea Kramer <aletheakramer@outlook.com>
2022-09-26 21:34:41 -0700

    Bug fix

--- src/components/Modal/DateModal.js
+++ src/components/Modal/DateModal.js
@@ -14,9 +14,9 @@ export default function Modal({param, submitButton}) {
     return ( 
         <div>
             <form onSubmit={onSubmit}>
-                <label htmlFor="my-modal-6" className="btn modal-button">Add Date</label>
+                <label htmlFor="my-modal-5" className="btn modal-button">Add Date</label>
 
-                <input type="checkbox" id="my-modal-6" className="modal-toggle" />
+                <input type="checkbox" id="my-modal-5" className="modal-toggle" />
                 <div className="box-content modal modal-bottom modal-middle">
                     <div className="modal-box w-80 h-96">
                         <ReactDatePicker
@@ -27,7 +27,7 @@ export default function Modal({param, submitButton}) {
                         <div className="">
                             <div className="modal-action absolute bottom-0 right-0 m-3">
                             <input type="submit" value="Add" className="btn btn-primary" /> 
-                            <label htmlFor="my-modal-6" className="btn">Close</label>
+                            <label htmlFor="my-modal-5" className="btn">Close</label>
                             </div>
                         </div>
                     </div>
