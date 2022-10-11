751ea1ae45920580b4fe8b2ab16e4f55bebfcc66
Alethea Kramer <aletheakramer@outlook.com>
2022-10-10 17:42:22 -0700

    Remove activity from day

---
+++ src/components/ItinieraryActivity.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Activities from './Activities';
+
+const ItineraryActicity = ({ index, activities, id, remove }) => {
+    const getName = (stringId) => {
+        const activity = activities.filter((item) => item._id === stringId)[0]
+        return activity["title"]
+    }
+
+    const getTime = (stringId) => {
+        const activity = activities.filter((item) => item._id === stringId)[0]
+        return activity["time"]
+    }
+
+    return (
+        <div className='flex ml-4'>
+            <div className='w-10 felx-none'>
+                <p className=''>{getTime(id)}</p>
+            </div>
+            <div className='vertical-line'></div>
+            <div className='flex-grow'>
+                <p className='inline-block'>{getName(id)}</p>
+            </div>
+            <div className='flex-none mr-4'>
+                <button onClick={() => remove(id, index)} className='btn btn-outline btn-circle btn-error btn-xs'>×</button>
+            </div>
+        </div>
+    )
+}
+
+export default ItineraryActicity;
