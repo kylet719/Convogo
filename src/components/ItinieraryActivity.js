11f5b09bb894e52f090e7ec4051d28545294cde7
Alethea Kramer <aletheakramer@outlook.com>
2022-10-16 23:36:54 -0700

    Completed CRUD, added 'details' to model, styling

--- src/components/ItinieraryActivity.js
+++ src/components/ItinieraryActivity.js
@@ -13,16 +13,18 @@ const ItineraryActicity = ({ index, activities, id, remove }) => {
     }
 
     return (
-        <div className='flex ml-4'>
-            <div className='w-10 flex-none'>
-                <p className='text-xs text-right mr-1'>{getTime(id)}</p>
+        <div className='flex ml-4 h-8'>
+            <div className='w-16 flex-none'>
+                <p className='text-xs text-right mr-2 mt-1'>{getTime(id)}</p>
             </div>
             <div className='vertical-line'></div>
             <div className='flex-grow'>
                 <p className='inline-block'>{getName(id)}</p>
             </div>
-            <div className='flex-none mr-4'>
-                <button onClick={() => remove(id, index)} className='btn btn-outline btn-circle btn-error btn-xs'>×</button>
+            <div className='flex-none px-3.5'>
+                <button onClick={() => remove(id, index)} className='btn btn-outline btn-circle btn-error btn-xs'>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                </button>
             </div>
         </div>
     )
--- src/components/Modal/ViewEditModal.js
+++ src/components/Modal/ViewEditModal.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import "./Modal.css";
 
-const ViewEditModal = ({ activity }) => {
+
+const ViewEditModal = ({ activity, updateActivity }) => {
+
+    const onSubmit = (e) => {
+        const location = document.getElementById('location').value == "" ? activity.location : document.getElementById('location').value;
+        const time = document.getElementById('time').value == "" ? activity.time : document.getElementById('time').value;
+        const title = document.getElementById('title').value == "" ? activity.title : document.getElementById('title').value;
+        const details = document.getElementById('details').value == "" ? activity.title : document.getElementById('details').value;
+        e.preventDefault()
+        updateActivity(activity._id, location, time, title, details);
+    }
 
     return (
         <div>
@@ -9,21 +19,35 @@ const ViewEditModal = ({ activity }) => {
             <input type="checkbox" id="my-modal-10" className="modal-toggle" />
             <div className="modal modal-bottom sm:modal-middle">
                 <div className="modal-box h-96 relative">
-                    <h3 className="text-6xl mt-2 ml-2 left absolute">{activity.title}</h3>
-                    <div class="absolute left-20 top-2 bottom-2 -ml-0.5 w-0.5 bg-gray-300"></div>
-                    <div className='absolute top-2 left-24 right-5'>
-                        <h3 className='uppercase text-xl font-medium mb-2'>Logistics</h3>
-                        <p className='text-sm mb-3'>Day: {new Date(activity.date).toString()}</p>
-                        <p className='text-sm mb-3 text-black'>Location:
-                            <input type="text" placeholder={activity.location} className="text-sm placeholder:text-sm placeholder:text-black focus:border-none focus:outline-none focus:ring-none input input-ghost input-xs w-full max-w-xs" />
-                        </p>
-                        <p className='text-sm mb-3'>Time:
-                            <input type="text" placeholder={activity.time} className="text-sm placeholder:text-sm placeholder:text-black focus:border-none focus:outline-none focus:ring-none input input-ghost input-xs w-full max-w-xs" />
-                        </p>
-                    </div>
-                    <div className="modal-action absolute bottom-2 right-2">
-                        <label htmlFor="my-modal-10" className="btn">Save</label>
-                    </div>
+                    <form onSubmit={onSubmit}>
+                        <h3 className="text-6xl mt-2 ml-2 left absolute">{activity.title}</h3>
+                        <div class="absolute left-20 top-2 bottom-2 -ml-0.5 w-0.5 bg-gray-300"></div>
+                        <div className='absolute top-2 left-24 right-5'>
+                            <h3 className='uppercase text-xl font-medium mb-2'>Logistics</h3>
+
+                            <p className='text-sm mb-3 text-black'>Title:
+                                <input type="text" id='title' placeholder={activity.title} className="text-sm placeholder:text-sm placeholder:text-black focus:border-none focus:outline-none focus:ring-none input input-ghost input-xs w-full max-w-xs" />
+                            </p>
+
+                            <p className='text-sm mb-3'>Day: {new Date(activity.date).toString().substring(0, 15)}</p>
+
+                            <p className='text-sm mb-3 text-black'>Location:
+                                <input type="text" id='location' placeholder={activity.location} className="text-sm placeholder:text-sm placeholder:text-black focus:border-none focus:outline-none focus:ring-none input input-ghost input-xs w-full max-w-xs" />
+                            </p>
+
+                            <p className='text-sm mb-3'>Time:
+                                <input type="text" id='time' placeholder={activity.time} className="text-sm placeholder:text-sm placeholder:text-black focus:border-none focus:outline-none focus:ring-none input input-ghost input-xs w-full max-w-xs" />
+                            </p>
+
+                            <h3 className='uppercase text-xl font-medium mb-2'>Details</h3>
+                            <textarea id='details' placeholder={activity.details} className="placeholder:text-black textarea textarea-bordered w-full overflow-y align-top overflow-visible" />
+
+                        </div>
+                        <div className="modal-action absolute bottom-2 right-2">
+                            <input type="submit" value="Save" className="btn btn-primary" />
+                            <label htmlFor="my-modal-10" className="btn">Close</label>
+                        </div>
+                    </form>
                 </div>
             </div>
 
