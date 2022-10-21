99a00a1c7e681c2771980952122c813e67520fd6
Alethea Kramer <aletheakramer@outlook.com>
2022-10-20 22:49:28 -0700

    Final Deployment Prep

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
@@ -9,6 +10,18 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, '..', 'client', '/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API running');
+  });
+}
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true }
@@ -30,13 +43,13 @@ app.use('/chats', chatRouter);
 app.use('/invitations', invitationRouter);
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
 
 
 //Socket.io stuff
 const http = require('http');
-const {Server} = require("socket.io");
+const { Server } = require("socket.io");
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
