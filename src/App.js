d7f53fae2d529963d21b008f6dd3bdf2133e6459
Kyle <kyletam0719@outlook.com>
2022-10-27 21:34:42 -0700

    Test with Hash Routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Event from "./components/Event";
 import Dashboard from "./components/Dashboard";
 import { DndProvider } from "react-dnd";
